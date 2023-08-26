2804aaf4fcaaedb80813d361e8282344c69c2974
sarahhamdii <saraelshobak2000@gmail.com>
2023-08-26 10:09:00 -0700

    6 task

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-for i in range (100):
-    if i < 10:
-        print("0{}".format(i) , end=", ")
-    elif i < 99:
-        print("{}".format(i), end=", ")
-    else:
-        print("99")
+for i in range (8):
+    for j in range(i + 1, 10):
+        print("{}{}".format(i, j), end=", ")
+print("89")
