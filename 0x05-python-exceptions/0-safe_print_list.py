d66461a9da5fc7786ce979f8ada199c9e3c9b766
sarahhamdii <saraelshobak2000@gmail.com>
2023-08-28 06:26:48 -0700

    0 task

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,6 +5,6 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_list[i]), end="")
         print("")
         return (x)
-    except:
+    except IndexError:
         print("")
         return (i)
