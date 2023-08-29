3d72a0428d2cbb141b06ef56bc229d2eba950b2a
sarahhamdii <saraelshobak2000@gmail.com>
2023-08-29 16:46:35 -0700

    6 task

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -6,41 +6,47 @@ class Square:
     '''defines a square'''
 
     def __init__(self, size=0, position=(0, 0)):
+        '''Instantiation'''
         self.size = size
         self.position = position
 
-    def area(self):
-        return self.__size**2
-
     @property
     def size(self):
+        '''to retrieve'''
         return self.__size
+
+    @property
+    def position(self):
+        '''to retrieve'''
+        return self.__position
 
     @size.setter
     def size(self, value):
+        '''property setter'''
         if type(value) != int:
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
-    def my_print(self):
-        ''' prints in stdout the square'''
-        if self.__size == 0:
-            print("")
-        for i in range(self.__position[1]):
-            print("")
-        for i in range(self.__size):
-            print(" " * self.__position[0] + "#" * self.__size)
-
-    @property
-    def position(self):
-        return self.__position
-
     @position.setter
     def position(self, value):
+        '''property setter'''
         if any(type(i) != int for i in value) or any(j < 0 for j in value):
             raise TypeError("position must be a tuple of 2 positive integers")
         if type(value) != tuple or len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
+
+    def area(self):
+        '''Public instance'''
+        return self.__size ** 2
+
+    def my_print(self):
+        ''' prints in stdout the square'''
+        if self.__size == 0:
+            print("")
+        for i in range(self.__position[1]):
+            print("\n")
+        for i in range(self.__size):
+            print(" " * self.__position[0] + "#" * self.__size)
