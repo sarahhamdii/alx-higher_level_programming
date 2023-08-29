e9dbafb374f88dacd3a19a25d9db49d6fca16759
sarahhamdii <saraelshobak2000@gmail.com>
2023-08-29 15:49:39 -0700

    6 task

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -6,8 +6,8 @@ class Square:
     '''defines a square'''
 
     def __init__(self, size=0, position=(0, 0)):
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
