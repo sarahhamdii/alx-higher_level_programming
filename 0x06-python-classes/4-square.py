d082d5c2ab8e89a8b44ec1eb666d0212372d839f
sarahhamdii <saraelshobak2000@gmail.com>
2023-08-29 14:45:51 -0700

    4 task

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -18,6 +18,6 @@ class Square:
         if (value < 0):
             raise ValueError("size must be >= 0")
         self.__size = value
-        
+
     def area(self):
         return ((self.__size) ** 2)
