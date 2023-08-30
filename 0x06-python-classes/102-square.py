f72cf68c89f90888c411c095c9c7e7e7e86ded7c
sarahhamdii <saraelshobak2000@gmail.com>
2023-08-29 17:39:53 -0700

    102 task

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+'''defines a square based on 4-square.py'''
+
+
+class Square:
+    '''defines a square'''
+    def __init__(self, size=0):
+        self.__size = size
+
+    def area(self):
+        return ((self.__size) ** 2)
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if (value < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def __eq__(self, other):
+        return self.area() == other.area()
+
+    def __ne__(self, other):
+        return self.area() != other.area()
+
+    def __lt__(self, other):
+        return self.area() < other.area()
+
+    def __gt__(self, other):
+        return self.area() > other.area()
+
+    def __le__(self, other):
+        return self.area() <= other.area()
+
+    def __ge__(self, other):
+        return self.area() >= other.area()
