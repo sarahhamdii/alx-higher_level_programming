69bd96a865dfc54c644afcadc2fd8503c8d44f19
sarahhamdii <saraelshobak2000@gmail.com>
2023-08-29 17:27:15 -0700

    101 task

---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+'''defines a square based on 6-square.py'''
+
+
+class Square:
+    '''defines a square'''
+    def __init__(self, size=0, position=(0, 0)):
+        '''Instantiation'''
+        self.size = size
+        self.position = position
+
+    def area(self):
+        '''Public instance'''
+        return ((self.__size) ** 2)
+
+    @property
+    def size(self):
+        '''to retrieve'''
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        '''property setter'''
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        """Prints the square"""
+        if self.__size == 0:
+            print()
+        else:
+            for y in range(self.__position[1]):
+                print()
+            for i in range(self.__size):
+                for x in range(self.__position[0]):
+                    print(' ', end='')
+                for j in range(self.__size):
+                    print('#', end='')
+                print()
+
+    @property
+    def position(self):
+        '''to retrieve'''
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        '''property setter'''
+        if type(value) != tuple or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if any(type(i) != int for i in value) or any(j < 0 for j in value):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def __str__(self):
+        '''same as print'''
+        output = []
+        if self.__size == 0:
+            output.append("")
+        else:
+            for i in range(self.__position[1]):
+                output.append("")
+            for i in range(self.__size):
+                output.append(" " * self.__position[0] + "#" * self.__size)
+        return "\n".join(output)
