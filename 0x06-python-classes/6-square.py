83e18023ddfbaa22f515ce5bcff08b4383307cca
sarahhamdii <saraelshobak2000@gmail.com>
2023-08-29 17:09:52 -0700

    6 task

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -34,7 +34,7 @@ class Square:
         for i in range(self.__position[1]):
             print("")
         for i in range(self.__size):
-            print("" * self.__position[0] + "#" * self.__size)
+            print(" " * self.__position[0] + "#" * self.__size)
 
     @property
     def position(self):
